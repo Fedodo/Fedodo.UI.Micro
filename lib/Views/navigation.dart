9711972608b6fd7aea1ee19911480d81e7cc7043
Lukas Nagel <lukas@lna-dev.net>
2023-01-31 20:05:03 +0100

    💄 Updated Icon

--- lib/Views/navigation.dart
+++ lib/Views/navigation.dart
@@ -44,7 +44,7 @@ class _NavigationState extends State<Navigation> {
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
           BottomNavigationBarItem(icon: Icon(Icons.notifications), label: "Notifications"),
-          BottomNavigationBarItem(icon: Icon(Icons.contacts), label: "Profile"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
         ],
       ),
       body: screens[currentIndex],
