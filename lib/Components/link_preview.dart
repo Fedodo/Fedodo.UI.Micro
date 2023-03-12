4b14ce107a95d48b0a513c36972e59669d315a2c
Lukas Nagel <lukas@lna-dev.net>
2023-03-12 16:56:33 +0100

    🎨 Code Cleanup

--- lib/Components/link_preview.dart
+++ lib/Components/link_preview.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:metadata_fetch/metadata_fetch.dart';
