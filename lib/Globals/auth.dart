5d56f80b05e4ed6e7e7a145dba1a24d796cae828
Lukas Nagel <lukas@lna-dev.net>
2023-05-12 12:22:34 +0200

    ✨ Updated redirect uri

--- lib/Globals/auth.dart
+++ lib/Globals/auth.dart
@@ -2,5 +2,5 @@ import 'global_settings.dart';
 
 class AuthGlobals{
   // static String redirectUriWeb = "localhost:37471/redirect.html";
-  static String redirectUriWeb = "https://${GlobalSettings.domainName}/redirect.html";
+  static String redirectUriWeb = "https://micro.${GlobalSettings.domainName}/redirect.html";
 }
