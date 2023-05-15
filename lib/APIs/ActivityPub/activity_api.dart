45cf34128653436c9ba8795895efdd844ea424e9
Lukas Nagel <lukas@lna-dev.net>
2023-05-15 19:44:24 +0200

    🐛 Fixed comments not loading

--- lib/APIs/ActivityPub/activity_api.dart
+++ lib/APIs/ActivityPub/activity_api.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:fedodo_micro/Extensions/url_extensions.dart';
 import 'package:fedodo_micro/Globals/global_settings.dart';
 import 'package:http/http.dart' as http;
 import '../../Models/ActivityPub/activity.dart';
@@ -59,8 +60,15 @@ class ActivityAPI {
   }
 
   Future<Activity<Post>> getActivity(String activityId) async {
+
+    Uri activityUri = Uri.parse(activityId);
+
+    if(activityUri.authority != GlobalSettings.domainName){
+      activityUri = activityUri.asProxyUri();
+    }
+
     http.Response response = await http.get(
-      Uri.parse(activityId),
+      activityUri,
       headers: <String, String>{
         "Accept": "application/json",
       },
