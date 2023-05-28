a5781357ea13a8a65a6bb4169dcfcf9a8a3a731c
Lukas Nagel <lukas@lna-dev.net>
2023-05-28 20:07:12 +0200

    🚧 Started SearchTab
    #31

--- lib/Views/NavigationViews/search.dart
+++ lib/Views/NavigationViews/search.dart
@@ -8,8 +8,37 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  final searchTextEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Text("Search");
+    return Column(
+      children: [
+        Form(
+          child: Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  controller: searchTextEditingController,
+                  decoration: const InputDecoration(
+                    hintText: 'Search',
+                  ),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.search),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
