0b92ff7daaff3c5d076bd12dd276ef59611f8900
Lukas Nagel <lukas@lna-dev.net>
2023-05-30 06:00:10 +0200

    🐛 Updated webfingerApi to proxy uri
    #31

--- lib/APIs/webfinger_api.dart
+++ lib/APIs/webfinger_api.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:fedodo_micro/Extensions/url_extensions.dart';
 import 'package:http/http.dart' as http;
 
 class WebfingerApi {
@@ -7,7 +8,7 @@ class WebfingerApi {
   Future<String> getUser(String input) async{
     String domain = input.split("@")[1];
 
-    var resp = await http.get(Uri.parse("https://$domain/.well-known/webfinger?resource=acct:$input"));
+    var resp = await http.get(Uri.parse("https://$domain/.well-known/webfinger?resource=acct:$input").asProxyUri());
 
     var result = jsonDecode(resp.body);
     var links = result["links"];
